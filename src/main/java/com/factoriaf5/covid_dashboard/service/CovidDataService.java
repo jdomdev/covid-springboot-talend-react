a5a3ee0082ddae84e5217c1c43bb6a1afafab7a4
jdomdev <jdomingodev@duck.com>
2025-08-22 01:41:23 +0200

    refactor(structure): remove redundant root src directory
    Finalize project refactoring by
         removing the superfluous top-level src/ directory. All source code now resides correctly within the 'covid-dashboard'
         module.

--- src/main/java/com/factoriaf5/covid_dashboard/service/CovidDataService.java
+++
@@ -1,9 +0,0 @@
-package com.factoriaf5.covid_dashboard.service;
-
-import java.util.List;
-
-import com.factoriaf5.covid_dashboard.dto.CovidDataDto;
-
-public interface CovidDataService {
-    List<CovidDataDto> getAll();
-}
